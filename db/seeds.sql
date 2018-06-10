abed903c3559071adf240f9e49085678eb500a76
Guan <heguanelvis@gmail.com>
2018-06-09 21:47:21 -0500

    created database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Spicy Chicken');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Double Cheese');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Smoked Bacon and Cheese');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Beef');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Barbeque Chicken');
